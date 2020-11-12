0ec61d245953481743765322a6d507dd79b557c7
Winston Chen <winstonchen.wm@gmail.com>
2020-11-12 04:37:02 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -59,14 +59,5 @@ end
 # get every test to pass before coding runner below #
 #####################################################
 
-def runner
-  # code runner here
-  welcome  
-  card_total = initial_round
-  while card_total < 21
-    card_total = hit?(card_total)
-    card_total = display_card_total(card_total)
-  end
-  end_game(card_total)
-end
+
     
